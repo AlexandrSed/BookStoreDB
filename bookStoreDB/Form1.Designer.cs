d2c6ebedea95a2f157749286a62db38581329435
SedovAlexandr <sedovaleksandr808@gmail.com>
2024-12-06 14:14:39 +0300

    made the output of data in window

--- bookStoreDB/Form1.Designer.cs
+++ bookStoreDB/Form1.Designer.cs
@@ -29,11 +29,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.booksInfoView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.booksInfoView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // booksInfoView
+            // 
+            this.booksInfoView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.booksInfoView.Location = new System.Drawing.Point(28, 38);
+            this.booksInfoView.Name = "booksInfoView";
+            this.booksInfoView.RowTemplate.Height = 24;
+            this.booksInfoView.Size = new System.Drawing.Size(760, 426);
+            this.booksInfoView.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.booksInfoView);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.booksInfoView)).EndInit();
+            this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.DataGridView booksInfoView;
 
         #endregion
     }
--- bookStoreDB/Form1.cs
+++ bookStoreDB/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace bookStoreDB
 {
@@ -15,6 +17,48 @@ namespace bookStoreDB
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MongoClient client = new MongoClient(Properties.Settings.Default.ConnString);
+            
+            var db = client.GetDatabase("bookstore");
+ 
+            IMongoCollection<BsonDocument> booksCollection = db.GetCollection<BsonDocument>("books");
+            
+            List<BsonDocument> books = booksCollection.Find(new BsonDocument()).ToList();
+
+            DataTable dataTable = new DataTable();
+
+            foreach (var book in books)
+            {
+                if (dataTable.Columns.Count == 0)
+                {
+                    foreach (var colName in book.Names)
+                    {
+                        if(colName != "_id")
+                            dataTable.Columns.Add(colName);
+                    }
+                }
+
+                dataTable.Rows.Add(new object[]
+                {
+                    book["title"], 
+                    book["author"],
+                    book["genre"],
+                    book["price"],
+                    book["pages"],
+                    book["publisher"],
+                    book["yearOfPublication"],
+                    book["ageLimit"],
+                    book["count"]
+                });
+            }
+
+            // Display the data from the data table in the data grid view.
+            this.booksInfoView.DataSource = dataTable;
+            
         }
     }
 }
